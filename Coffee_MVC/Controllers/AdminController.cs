910a37bfe865642738768b67d28c3bac7e856ab2
ksapapultsev_laptop <ksarapultsev2014@gmail.com>
2025-06-18 10:17:30 +0600

    Add controller, view Admin

--- Coffee_MVC/Controllers/AdminController.cs
+++ Coffee_MVC/Controllers/AdminController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Coffee_MVC.Controllers
 {
+    [Authorize(Roles="Administrator")]
     public class AdminController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Users()
+        {
+            var listUsers = new List<string>();
+            return View(listUsers);
         }
     }
 }
